c682d8f869d6c099dfda80c33743654a21afd6f2
Connor Collins <connor.collins@gmail.com>
2013-04-26 22:55:01 -0400

    skipped ahead a bit, as I was reading about cryptographic systems today and thought chapter 9 might help me understand it better.

---
+++ 9.1-hashlib.py
@@ -0,0 +1,12 @@
+## hashlib
+ # hashlib is the core of Python cryptography, and includes many popular encryption algorithms
+
+import hashlib
+
+lorem = '''Lorem ipsum dolor sit amet, consectetur adispisicing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+ut aliquip ex ea commodo consequat.  Duis aute irure dolor in
+reprehenderit in voluptate velit ease cillum dolore eu fugiat nulla
+pariatur. Exceptur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.'''
