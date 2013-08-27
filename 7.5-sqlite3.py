c7e36f67e9f79c2c5c2c5c252759ca2efcd03f72
Connor Collins <connor.collins@gmail.com>
2013-08-27 07:05:24 -0400

    Rewrote 7.5-sqlite3 so that all examples are encapsulated in functions.  The Function Controls section at the end of the file defines their usage.

--- 7.5-sqlite3.py
+++ 7.5-sqlite3.py
@@ -23,7 +23,6 @@ from contextlib import closing
 # or it can be loaded from memory
         
 schema = """-- Schema for to-do application examples
-
 --projects are high-level activities made up of tasks
 create table project (
     name        text    primary key,
@@ -47,115 +46,110 @@ create table task (
 task_fmt = '{:2d} ({:d}) {:50s} [{:8s}] ({:s})'
 
 ## 7.5.1 Creating a Database
-# An SQLite db is stored as a single file on the file system
-db_filename = 'data/7.5-sqlite3_to-do.db'
-db_is_new = not os.path.exists(db_filename)
+def create_new_database( db_filename, schema_filename, add_starter_data=False ):
+    # An SQLite db is stored as a single file on the file system
+    db_is_new = not os.path.exists(db_filename)
+    schema_is_new = not os.path.exists(schema_filename)
 
-schema_filename = 'data/7.5-sqlite3_to-do_schema.sql'
-schema_is_new = not os.path.exists(schema_filename)
+    if db_is_new or schema_is_new:
+        with closing( sqlite3.connect(db_filename) ) as conn:
+            if db_is_new:
+                print "Database was created."   
+            else:
+                print "Database already exists."
 
-with closing( sqlite3.connect(db_filename) ) as conn:
-    if db_is_new:
-        print "Database was created."   
-    else:
-        print "Database already exists."
-
-    if schema_is_new:
-        # The next step is creating a schema
-
-        with closing( open(schema_filename, 'w+' ) ) as schema_file:
-            schema_file.write(schema)
-        print "  Schema was created."
-    else: 
-        print "  Schema already exists."
-print
-
-# apply the schema (from file, in this case)
-# [though I'm overwriting the variable which already has the schema so this is not exactly useful]
-with closing( sqlite3.connect(db_filename) ) as conn:
-    if (db_is_new or schema_is_new) :
-        print "Applying Schema"
-        with closing( open(schema_filename, 'r') ) as f:
-            schema = f.read()
-        # however you obtain the schema data, run it with executescript() on the db
-        conn.executescript(schema)
-        print " Schema Applied"
+            if schema_is_new:
+                # The next step is creating a schema
+                with closing( open(schema_filename, 'w+' ) ) as schema_file:
+                    schema_file.write(schema)
+                print "  Schema was created."
+            else: 
+                print "  Schema already exists."
         print
 
-        # since this block is only called when one/two of the two things required before you can insert data
-        # did not exist prior to this instance of code, which means this data needs to get shovelled in.
-        data = """
-        insert into project ( name, description, deadline )
-        values ( 'pystl', 'Python Standard Library by Example', '2013-08-31' );
+    # apply the schema (from file, in this case)
+    # [though I'm overwriting the variable which already has the schema so this is not exactly useful]
+    with closing( sqlite3.connect(db_filename) ) as conn:
+        if (db_is_new or schema_is_new) :
+            print "Applying Schema"
+            with closing( open(schema_filename, 'r') ) as f:
+                schema = f.read()
+            # however you obtain the schema data, run it with executescript() on the db
+            conn.executescript(schema)
+            print " Schema Applied"
+            print
+            
+            if add_starter_data:
+                # since this block is only called when one/two of the two things required before you can insert data
+                # did not exist prior to this instance of code, which means this data needs to get shovelled in.
+                data = """
+                insert into project ( name, description, deadline )
+                values ( 'pystl', 'Python Standard Library by Example', '2013-08-31' );
 
-        insert into task ( details, status, deadline, project )
-        values ( 'Chapter 6', 'done', '2013-08-25', 'pystl' );
+                insert into task ( details, status, deadline, project )
+                values ( 'Chapter 6', 'done', '2013-08-25', 'pystl' );
 
-        insert into task ( details, status, deadline, project )
-        values ( 'Chapter 7', 'pending', '2013-08-26', 'pystl' );
+                insert into task ( details, status, deadline, project )
+                values ( 'Chapter 7', 'pending', '2013-08-26', 'pystl' );
 
-        insert into task ( details, status, deadline, project )
-        values ( 'Chapter 8', 'new', '2013-08-27', 'pystl' );        
-        """ 
-        with closing( sqlite3.connect(db_filename) ) as conn:
-            print "Inserting Initial Data"
-            conn.executescript(data)
+                insert into task ( details, status, deadline, project )
+                values ( 'Chapter 8', 'new', '2013-08-27', 'pystl' );        
+                """ 
+                with closing( sqlite3.connect(db_filename) ) as conn:
+                    print "Inserting Starter Data"
+                    conn.executescript(data)
     
 ## 7.5.2 Retrieving Data
 # to retrieve values, create a cursor from a database connection.
 # A cursor provides a consistent view of data and is the primary means or transacting with a relational db
-
-with closing( sqlite3.connect(db_filename) ) as conn:
-    # create cursor
-    cursor = conn.cursor()
-    # execute query through cursor
-    cursor.execute("""
-    select id, priority, details, status, deadline from task where project = 'pystl'
-    """)
-    # read data from cursor
-    for row in cursor.fetchall():
-        task_id, priority, details, status, deadline = row
-        print task_fmt.format( task_id, priority, details, status, deadline )
-    print
-    
-    # there`s also a fetchone() that just fetches the first result,
-    cursor.execute("""
-    select name, description, deadline from project
-    """)
-    
-    project_name, description, deadline = cursor.fetchone()
-    print "Project details for %s (%s) [%s]" % ( description, project_name, deadline )
-    
-    # and a fetchmany(x) that just fetches [up to] x results.
-    cursor.execute("""
-    select id, priority, details, status, deadline from task 
-    where project = ?
-    """, ( project_name, ) )
-    print "Two results:"
-    for row in cursor.fetchmany(2):
-        task_id, priority, details, status, deadline = row
-        print task_fmt.format( int(task_id), int(priority), details, status, deadline )
-    print "Five more results:"    
-    for row in cursor.fetchmany(5):
-        task_id, priority, details, status, deadline = row
-        print task_fmt.format( int(task_id), int(priority), details, status, deadline )
+def fetch_project_tasks(project_name, fetch_type = 'all'):
+    with closing( sqlite3.connect(db_filename) ) as conn:
+        # create cursor
+        cursor = conn.cursor()
+        # execute query through cursor
+        cursor.execute("""
+            select id, priority, details, status, deadline from task where project = ?
+            """, (project_name, ) )
+            
+        if fetch_type == 'all':
+            # read data from cursor
+            for row in cursor.fetchall():
+                task_id, priority, details, status, deadline = row
+                print task_fmt.format( task_id, priority, details, status, deadline )
+            print
         
+        elif fetch_type == 'one':
+            # there`s also a fetchone() that just fetches the first result,
+            for row in cursor.fetchone():
+                task_id, priority, details, status, deadline = row
+                print task_fmt.format( task_id, priority, details, status, deadline )
+            print
+            
+        elif fetch_type == 'many':
+            # and a fetchmany(x) that just fetches [up to] x results.
+            how_many = 5
+            print how_many, "results:"
+            for row in cursor.fetchmany( how_many ):
+                task_id, priority, details, status, deadline = row
+                print task_fmt.format( task_id, priority, details, status, deadline )
+            
 ## 7.5.3 Query Metadata
 # after execute() has been called, the cursor sets it's description attribute 
 # to hold info about the data that will be returned by the fetch methods.
 # the description = ( (column_name, type, display_size, internal_size, precision, scale, null_flag), ...)
 # though, sqlite3 only returns the name, because it doesn't enforce size or type restraints
-with closing( sqlite3.connect(db_filename) ) as conn:
-    cursor = conn.cursor()
-    
-    cursor.execute("""
-    select * from task where project = ?
-    """, ( project_name, ) )
-    
-    print "\nTask table has these columns:"
-    for colinfo in cursor.description:
-        print colinfo
-    print
+def display_columns(db_filename, project_name):
+    with closing( sqlite3.connect(db_filename) ) as conn:
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        select * from task where project = ?
+        """, ( project_name, ) )
+        
+        print "\nTask table for %s has these columns:" % project_name
+        for colinfo in cursor.description:
+            print colinfo
+        print
         
 ## 7.5.4 Row Objects
 # the default rows returned by the fetch methods are tuples
@@ -165,30 +159,31 @@ with closing( sqlite3.connect(db_filename) ) as conn:
 
 # Connection objects have a row_factory property that allows the calling code to control the type of object representing each row
 # sqlite3 also includes a Row class intended to be used as a row_factory (wow)
-with closing( sqlite3.connect(db_filename) ) as conn:
-    # Change the row_factory to use sqlite3.Row
-    conn.row_factory = sqlite3.Row
-    
-    cursor = conn.cursor()
-    
-    cursor.execute("""
-    select name, description , deadline from project
-    where name = ?
-    """, ( project_name, ) )
-    p_name, description, deadline = cursor.fetchone()
-    print "Project details for %s (%s) [%s]" % ( description, project_name, deadline )
-    
-    cursor.execute("""
-    select id, priority, details, status, deadline from task 
-    where project = ?
-    order by deadline
-    """, ( p_name, ) )
-    
-
-    for row in cursor.fetchmany(5):
-        # read data from cursor using the Row object
-        print task_fmt.format(  row['id'], row['priority'], row['details'], row['status'], row['deadline'] )
-    print
+def display_rows_via_objects(db_filename, project_name):
+    with closing( sqlite3.connect(db_filename) ) as conn:
+        # Change the row_factory to use sqlite3.Row
+        conn.row_factory = sqlite3.Row
+        
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        select name, description , deadline from project
+        where name = ?
+        """, ( project_name, ) )
+        
+        p_name, description, deadline = cursor.fetchone()
+        print "Project details for %s (%s) [%s]" % ( description, p_name, deadline )
+        
+        cursor.execute("""
+        select id, priority, details, status, deadline from task 
+        where project = ?
+        order by deadline
+        """, ( p_name, ) )
+        
+        for row in cursor.fetchmany(5):
+            # read data from cursor using the Row object
+            print task_fmt.format(  row['id'], row['priority'], row['details'], row['status'], row['deadline'] )
+        print
 ## 7.5.5 Using Variables with Queries
 # the proper way to use dynamic values in queries is with host variables
 # Positional Arguments are indicated must match in number to the arguments provided
@@ -196,43 +191,36 @@ with closing( sqlite3.connect(db_filename) ) as conn:
     # Positional arguments are indicated with ?s
     
 # Named Parameters are useful in more complex queries and especially when values are used more than once
-    # Names parameters are indicated like :param_name
-with closing( sqlite3.connect(db_filename) ) as conn:
-    # Change the row_factory to use sqlite3.Row
-    conn.row_factory = sqlite3.Row
-    
-    cursor = conn.cursor()
-    
-    cursor.execute("""
-    select name, description , deadline from project
-    where name = ?
-    """, ( project_name, ) )
-    p_name, description, deadline = cursor.fetchone()
-    print "Project details for %s (%s) [%s]" % ( description, project_name, deadline )
-    
-    cursor.execute("""
-    select id, priority, details, status, deadline from task 
-    where project = :project_name
-    order by deadline
-    """, {'project_name':p_name} )  # see, here is the named parameter in use
+def named_parameters(db_filename, project_name):
+        # Names parameters are indicated like :param_name
+    with closing( sqlite3.connect(db_filename) ) as conn:
+        # Change the row_factory to use sqlite3.Row
+        conn.row_factory = sqlite3.Row
+        
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        select name, description , deadline from project
+        where name = ?
+        """, ( project_name, ) )
+        p_name, description, deadline = cursor.fetchone()
+        print "Project details for %s (%s) [%s]" % ( description, project_name, deadline )
+        
+        cursor.execute("""
+        select id, priority, details, status, deadline from task 
+        where project = :project_name
+        order by deadline
+        """, {'project_name':p_name} )  # see, here is the named parameter in use
 
-    for row in cursor.fetchmany(5):
-        # read data from cursor using the Row object
-        print task_fmt.format(  row['id'], row['priority'], row['details'], row['status'], row['deadline'] )   
-    print
+        for row in cursor.fetchmany(5):
+            # read data from cursor using the Row object
+            print task_fmt.format(  row['id'], row['priority'], row['details'], row['status'], row['deadline'] )   
+        print
 
-# check for special commands as arguments
-arg_command = sys.argv[1] if len(sys.argv) >= 2 else None
-if arg_command is None:
-    pass
-    
 # query parameters can be used with select, insert, and update statements, as long as a literal value is legal in the context
-elif arg_command == 'update':  # could also read `def update_task_status(id, status):` and then pass in those values to do that
+def update_task_status(id, status):
     # run as `python 7.5-sqlite3.py update id status`
-    id = int( sys.argv[2] )
-    status = sys.argv[3]
-
-    
+   
     with closing( sqlite3.connect(db_filename) ) as conn:
         cursor = conn.cursor()
         
@@ -256,9 +244,9 @@ elif arg_command == 'update':  # could also read `def update_task_status(id, sta
 ## 7.5.6 Bulk Loading
 # use executemany() to apply the same set of SQL instructions to a large set of data
 # this avoids complicating the code with loops and lets the underlying library apply loop optimization
-elif arg_command == 'insert':
+
+def insert_data_from_csv(data_filename):
     #  run as `python 7.5-sqlite3.py insert data_file.csv`
-    data_filename = sys.argv[2]
     SQL = """
     insert into task (details, priority, status, deadline, project)
     values (:details, :priority, :status, :deadline, :project)
@@ -273,5 +261,76 @@ elif arg_command == 'insert':
             cursor.executemany(SQL, csv_reader)
             
             conn.commit()
-                
-print "Completed assessing arguments."
+               
+
+## 7.5.7 Defining New Column Types
+# SQLite only supports integer, floating point, or text columns
+# sqlite3 has the ability to store any type of Python data
+
+# conversion for types supported beyond the base three is enabled in the db connection by using the detect_types flag.
+# if the columns were declared using the desired type when the table was defined, PARSE_DECLTYPES will provide them
+
+
+
+## Function Controls
+if __name__ == '__main__':
+    # check for special commands as arguments
+    arg_command = sys.argv[1] if len(sys.argv) >= 2 else None
+    if arg_command is None:
+        print "No command entered"
+    elif arg_command == 'create_db':
+        # create a new database file using the supplied file locations
+        # if any filename that doesn't exist is given, it will be created
+        # if a schema filename that doesn't exist is given the schema from the book will be created
+        db_filename = sys.argv[2]
+        schema_filename = sys.argv[3]
+        create_new_database( db_filename, schema_filename )
+    elif arg_command == 'create_demo_db':
+        # if you want the database to be populated with sample data matching the book's schema use this function instead
+        db_filename = sys.argv[2]
+        schema_filename = sys.argv[3]
+        create_new_database( db_filename, schema_filename, True )
+        
+    elif arg_command == 'columns':
+        # display the columns in the task table.
+        try:
+            db_filename = sys.argv[2]
+        except IndexError:
+            print "You must specify the db to read from"
+            raise
+        try:
+            project_name = sys.argv[3]  ## this doesn't really make sense but it could with a little work
+        except IndexError:
+            print "You must specify the project name"
+            raise
+        display_columns( db_filename, project_name )
+        
+    elif arg_command == 'object_rows':
+        # display the rows, like fetch, but using objects instead of tuples.
+        db_filename = sys.argv[2]
+        project_name = sys.argv[3]
+        display_rows_via_objects( db_filename, project_name )
+        
+    # there are a few options when it comes to asking for the fetch commands
+    elif arg_command == 'fetch' and len(sys.argv) == 3:
+        # default to fetchall() if no second argument is passed
+        project_name = sys.argv[2]
+        fetch_project_tasks( project_name, 'all' )
+    elif arg_command == 'fetch':
+        # otherwise, pass in the second argument as the fetch_type
+        project_name = sys.argv[2]
+        fetch_type = sys.argv[3]
+        fetch_project_tasks( project_name, fetch_type )
+    
+    elif arg_command == 'insert':
+        # insert the data from the csv file specified
+        csv_file_location = sys.argv[2]
+        insert_data_from_csv( csv_file_location )
+        
+    elif arg_command == 'update':
+        # update the status of the indicated task
+        task_id = int( sys.argv[2] )
+        new_task_status = sys.argv[3]
+        update_task_status( task_id, new_task_status )
+    else:
+        print "Command not recognized."
