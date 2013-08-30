cbd0d6e1a328a2ff08fc21712540de66b6e0be22
Connor Collins <connor.collins@gmail.com>
2013-08-30 07:22:55 -0400

    Added some spacing, subtitle comments for the sections, etc. Reading through this gave me an idea for an application to handle parking fees.

--- 9.2-hmac.py
+++ 9.2-hmac.py
@@ -1,15 +1,19 @@
-## hmac!
- # The HMAC algorithm is used to verify the integrity of messages passed between
- # applications or stored in potentially vulnerable locations.
-
+## 9.2 hmac - Cryptographic Signing and Verification
+# The HMAC algorithm is used to verify the integrity of messages passed between
+# applications or stored in potentially vulnerable locations.
 import hmac, hashlib, base64, pprint
-from StringIO import StringIO
+
+try:
+	from cStringIO import StringIO
+except:
+	from  StringIO import StringIO
 
 try:
     import cPickle as pickle
 except:
     import pickle
 
+## 9.2.1 Signing Messages
 digest_maker = hmac.new('secret-shared=key-goes-here')
 
 with open('data/lorem.txt', 'rb') as f:
@@ -22,11 +26,12 @@ with open('data/lorem.txt', 'rb') as f:
 digest = digest_maker.hexdigest()
 print '          MD5 digest:', digest
 
- # of course, SHA-1 is preferred over MD5
- # the new() method takes three arguments
-     # 1) The secret key
-     # 2) An initial message, or very small complete message
-     # 3) digest module to use, MD5 is the default
+## 9.2.2 SHA vs. MD5
+# of course, SHA-1 is preferred over MD5
+# the new() method takes three arguments
+	# 1) The secret key
+	# 2) An initial message, or very small complete message
+	# 3) digest module to use, MD5 is the default
 
 digest_maker = hmac.new('secret-shared-key-goes-here',
                         '',
@@ -37,21 +42,22 @@ with open('data/lorem.txt', 'rb') as f:
         block = f.read(1024)
         if not block:
             break
-        digest_maker.update(block)
+        digest_maker.update( block )
 
 digest = digest_maker.hexdigest()
 print '         SHA1 digest:', digest
 
- # sometimes, the base63 encoded binary digest is required, rather than the hexdigest
+## 9.2.3 Binary Digests
+# sometimes, the base64 encoded binary digest is required, rather than the hexdigest
 
 with open('data/lorem.txt', 'rb') as f:
     body = f.read()
 
 hash = hmac.new('secret-shared-key-goes-here', body, hashlib.sha1)
 digest = hash.digest()
-print 'Base64 binary digest:', base64.encodestring(digest)
+print 'Base64 binary digest:', base64.encodestring( digest )
 
-## Applications of Message Signatures
+## 9.2.4 Applications of Message Signatures
 
 def make_digest(message):
     """Return a digest for the message"""
@@ -67,50 +73,49 @@ class SimpleObject(object):
     def __str__(self):
         return self.name
 
- # Simulate a wriatble socket or pipe with StringIO
+ # Simulate a writeable socket or pipe with StringIO
 out_s = StringIO()
 
  # write a valid object to the stream
      # digest\nlength\npickle
 o = SimpleObject('Digest matches')
 pickled_data = pickle.dumps(o)
-digest = make_digest(pickled_data)
-header = '%s %s' % (digest, len(pickled_data))
+digest = make_digest( pickled_data )
+header = '%s %s' % ( digest, len(pickled_data) )
 print '\nWRITING:', header
-out_s.write(header + '\n' )
-out_s.write(pickled_data)
+out_s.write( header + '\n' )
+out_s.write( pickled_data )
 
- # write an invalid object to the stream
+# write an invalid object to the stream
 o = SimpleObject('digest does not match')
 pickled_data = pickle.dumps(o)
 digest = make_digest('this is not the pickled data at all')
-header = '%s %s' % (digest, len(pickled_data))
+header = '%s %s' % ( digest, len(pickled_data) )
 print '\nWRITING:', header
-out_s.write(header + '\n' )
-out_s.write(pickled_data)
+out_s.write( header + '\n' )
+out_s.write( pickled_data )
 
 out_s.flush()
 
- # Simulate a readable socket or pipe with StringIO
+# Simulate a readable socket or pipe with StringIO
 in_s = StringIO(out_s.getvalue())
-
 # Read the data
 while True:
     first_line = in_s.readline()
     if not first_line:
         break
     incoming_digest, incoming_length = first_line.split(' ')
-    incoming_length = int(incoming_length)
+    incoming_length = int( incoming_length )
     print '\nREAD:', incoming_digest, incoming_length
 
-    incoming_pickled_data = in_s.read(incoming_length)
-    actual_digest = make_digest(incoming_pickled_data)
+    incoming_pickled_data = in_s.read( incoming_length )
+    actual_digest = make_digest( incoming_pickled_data )
     print 'ACTUAL:', actual_digest
 
     if incoming_digest != actual_digest:
         print 'WARNING: Data corruption'
     else:
-        obj = pickle.loads(incoming_pickled_data)
+        obj = pickle.loads( incoming_pickled_data )
         print 'OK:', obj
         
     
