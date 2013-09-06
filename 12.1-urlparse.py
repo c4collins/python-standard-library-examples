fc68ee9f83acf5f0f9cecd5b94ba3520b3ba2298
Connor Collins <connor.collins@gmail.com>
2013-09-06 09:40:20 -0400

    Completed 12.1-urlparse

--- 12.1-urlparse.py
+++ 12.1-urlparse.py
@@ -35,15 +35,20 @@ def log_parsed_url( parsed ):
             logger.info( "%-8s: %s", attr, getattr(parsed, attr) )
     return
 
+## Constants
 simple_url = "http://NetLoc/path;param?query=arg#frag"
 complex_url = "http://user:pwd@NetLoc:80/p1;param/p2;param?query-arg#frag"
+extra_url = "http://NetLoc/path;?#"
+base_url = "http://www.example.com/path/file.html"
 
-if results.section in [0,1,2]:
+if results.section in [0,1,2,3]:
 
     ## 12.1.1 Parsing
     if results.section == 1 or results.section == 0:
         logger = logging.getLogger("12.1.1 Parsing")
-        # The return value from urlparse() in an object that acts like a tuple with 6 elements
+        logger.info("Showing: 12.1.1 Parsing")
+       
+       # The return value from urlparse() in an object that acts like a tuple with 6 elements
         logger.debug( "urlparse.urlparse( )" )
         logger.debug( "urlparse.urlparse( %s )", simple_url )
         log_parsed_url( urlparse.urlparse(simple_url) )
@@ -52,6 +57,7 @@ if results.section in [0,1,2]:
         logger.debug( "urlparse.urlsplit( )" )
         logger.debug( "urlparse.urlsplit( %s )", complex_url )
         log_parsed_url( urlparse.urlsplit( complex_url ) )
+        # urlsplit() returns an object that acts like a tuple with 5 elements, as there are no params
      
         # To simply string the gragment identifier from a URL, use urldefrag()
         url, fragment = urlparse.urldefrag(simple_url)
@@ -63,10 +69,42 @@ if results.section in [0,1,2]:
     ## 12.1.2 Unparsing
     if results.section == 2 or results.section == 0:
         logger = logging.getLogger("12.1.2 Unparsing")
+        logger.info("Showing: 12.1.2 Unparsing")
+        
         # There are several ways to assemble the parts of a split URL back into a single string.
         # The parsed URL has a geturl() method
         logger.debug( "Original: %s", simple_url )
         logger.debug( "Parsed  : %s", urlparse.urlparse( simple_url ).geturl()  )
+        # geturl() only works on an object returned by urlparse() or urlsplit()
+        
+        # A regular tuple containing strings can be combined into a URL with urlunparse()
+        logger.debug( "Original: %s", simple_url )
+        parsed = urlparse.urlparse( simple_url )
+        logger.debug( "Parsed  : %s [%s]", parsed, type(parsed) )
+        t = parsed[:]
+        logger.debug( "Tuple   : %s [%s]", t, type( t ) )
+        logger.debug( "Built   : %s", urlparse.urlunparse( t ) )
+        
+        # If the input URL has superfluous parts, they may be dropped when reconstructed
+        logger.debug( "Original: %s", extra_url )
+        parsed = urlparse.urlparse( extra_url )
+        logger.debug( "Parsed  : %s [%s]", parsed, type(parsed) )
+        t = parsed[:]
+        logger.debug( "Tuple   : %s [%s]", t, type( t ) )
+        logger.debug( "Built   : %s", urlparse.urlunparse( t ) )
+        
+    ## 12.1.3 Joining
+    if results.section == 3 or results.section == 0:
+        logger = logging.getLogger("12.1.3 Joining")
+        logger.info("Showing: 12.1.3 Joining")
+        
+        # In addition to parsing URLs, urljoin() will construct absolute URLs from relative fragments
+        logger.debug( "Base URL: %s", base_url )
+        logger.debug( "Add File: %s", urlparse.urljoin( base_url, "anotherfile.html" )  )
+        logger.debug( "Another : %s", urlparse.urljoin( base_url, "../anotherfile.html" )  )
+        # Note that urljoin() respects and interprets '../' correctly
+        logger.debug( "With /  : %s", urlparse.urljoin( base_url, "/subpath/thirdfile.html" )  )
+        logger.debug( "Without : %s", urlparse.urljoin( base_url, "subpath/fourthfile.html" )  )
     
 else:
     # If the command isn't recognized because it wasn't given, show the help.
